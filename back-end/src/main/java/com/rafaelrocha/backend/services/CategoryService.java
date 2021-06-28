54e16456eefe88f328a24eb5888c74d9a0661667
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-06-28 16:28:34 -0300

    Implemented Search by Id and custom expection NOT FOUND

--- back-end/src/main/java/com/rafaelrocha/backend/services/CategoryService.java
+++ back-end/src/main/java/com/rafaelrocha/backend/services/CategoryService.java
@@ -1,11 +1,16 @@
 package com.rafaelrocha.backend.services;
 
+import com.rafaelrocha.backend.dto.CategoryDTO;
 import com.rafaelrocha.backend.entities.Category;
 import com.rafaelrocha.backend.repositories.CategoryRepository;
+import com.rafaelrocha.backend.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -13,8 +18,18 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> categoryList = categoryRepository.findAll();
+
+        return categoryList.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> categoryObj = categoryRepository.findById(id);
+        Category category = categoryObj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(category);
     }
 
 }
