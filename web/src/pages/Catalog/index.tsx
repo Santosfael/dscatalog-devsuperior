92c1e2c99f91b094c49828f155f26b138013158b
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-07-28 14:38:45 -0300

    Finish product listing layout

--- web/src/pages/Catalog/index.tsx
+++ web/src/pages/Catalog/index.tsx
@@ -1,9 +1,21 @@
-
+import ProductCard from './components/ProductCard';
+import './styles.scss';
 
 function Catalog() {
     return (
-        <div>
-            <h1>Catalog</h1>
+        <div className="catalog-container">
+            <h1 className="catalog-title">Catálogo de produtos</h1>
+            <div className="catalog-product">
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+            </div>
         </div>
     )
 }
