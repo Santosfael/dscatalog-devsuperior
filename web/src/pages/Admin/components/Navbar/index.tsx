2df413dc5629a9a873806239c41d9bd0b6ce2898
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-07-30 11:11:31 -0300

    Add Admin Navbar

---
+++ web/src/pages/Admin/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import './styles.scss';
+
+function Navbar() {
+    return (
+        <nav className={"admin-nav-container"}>
+            <ul>
+                <li>
+                    <a href="*" className="admin-nav-item active">Meus Produtos</a>
+                </li>
+                <li>
+                    <a href="*" className="admin-nav-item">Minhas Categorias</a>
+                </li>
+                <li>
+                    <a href="*" className="admin-nav-item">Meus Usuários</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
--- web/src/pages/Admin/index.tsx
+++ web/src/pages/Admin/index.tsx
@@ -1,8 +1,10 @@
-
+import Navbar from './components/Navbar';
+import './styles.scss';
 
 function Admin() {
     return (
         <div>
+            <Navbar />
             <h1>Admin</h1>
         </div>
     )
