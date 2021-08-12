2407ebe497962831f15d5c18c51734178f086fba
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-08-12 16:58:32 -0300

    Search creation JQPL Product

--- back-end/src/main/java/com/rafaelrocha/backend/resources/ProductResource.java
+++ back-end/src/main/java/com/rafaelrocha/backend/resources/ProductResource.java
@@ -22,6 +22,8 @@ public class ProductResource {
 
     @GetMapping
     public ResponseEntity<Page<ProductDTO>> findAll(
+            @RequestParam(value = "categoryId", defaultValue = "0") Long categoryId,
+            @RequestParam(value = "name", defaultValue = "") String name,
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
             @RequestParam(value = "direction", defaultValue = "ASC") String direction,
@@ -30,7 +32,7 @@ public class ProductResource {
 
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 
-        Page<ProductDTO> productList = productService.findAllPaged(pageRequest);
+        Page<ProductDTO> productList = productService.findAllPaged(categoryId, name.trim(), pageRequest);
         return ResponseEntity.ok().body(productList);
     }
 
