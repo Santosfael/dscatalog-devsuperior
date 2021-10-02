03e50265e2ad030e8471ffbc9b6aa3e183de7b13
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-10-02 14:39:42 -0300

    Finish admin product card layout and listing back-end products

--- web/src/pages/Admin/components/Products/Card/index.tsx
+++ web/src/pages/Admin/components/Products/Card/index.tsx
@@ -1,25 +1,51 @@
+import ProductPrice from 'core/components/ProductPrice';
+import { Product } from 'core/types/Product';
 import './styles.scss';
 
-function Card() {
+type Props = {
+    product: Product;
+}
+
+function Card({ product }: Props) {
     return (
         <div className="card-base product-card-admin">
             <div className="row">
                 <div className="col-2 text-center border-end py-3">
                     <img
-                        src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"
-                        alt="Produto teste"
+                        src={product.imgUrl}
+                        alt={product.name}
                         className="product-card-image-admin"
                     />
                 </div>
                 <div className="col-7 py-3">
                     <h3 className="product-card-name-admin">
-                        Computador Desktop
+                        {product.name}
                     </h3>
+                    <ProductPrice price={product.price} />
+                    {
+                        product.categories.map((category) => (
+                            <span className="badge rounded-pill bg-secondary me-2" key={category.id}>
+                                {category.name}
+                            </span>
+                        ))
+                    }
                 </div>
-                <div className="col-3">
-                    <h1>
-                        Ações
-                    </h1>
+                <div className="col-3 pt-3 pe-5">
+                <div className="d-grid gap-2">
+                    <button
+                        type="button"
+                        className="btn btn-outline-secondary border-radius-10 mb-3 btn-edit"
+                    >
+                        EDITAR
+                    </button>
+
+                    <button
+                        type="button"
+                        className="btn btn-outline-danger border-radius-10"
+                    >
+                        EXCLUIR
+                    </button>
+                </div>
                 </div>
             </div>
         </div>
--- web/src/pages/Admin/components/Products/List/index.tsx
+++ web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,14 +1,38 @@
 import { useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import { ProductResponse } from "core/types/Product";
+import Pagination from "core/components/Pagination";
+import { Api } from "core/utils/api";
 
 import Card from "../Card";
 import './styles.scss';
 
 function List() {
     const history = useHistory();
+    const [productResponse, setProductResponse] = useState<ProductResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
+
+    console.log(productResponse);
 
     function handleCreate() {
         history.push('/admin/products/create');
     };
+
+    useEffect(() => {
+        const params = {
+            page: activePage,
+            linesPerPage: 4
+        }
+        setIsLoading(true);
+        Api({
+            url: '/products', params
+        }).then(response => setProductResponse(response.data))
+          .finally(()=> {
+            setIsLoading(false);
+          });
+    },[activePage]);
 
     return (
         <div className="admin-product-list">
@@ -17,9 +41,20 @@ function List() {
             </button>
 
             <div className="admin-list-container">
-                <Card />
-                <Card />
-                <Card />
+                {
+                    productResponse?.content.map((product) => (
+                        <Card product={product} key={product.id} />
+                    ))
+                }
+                {
+                productResponse && (
+                    <Pagination 
+                        totalPages={productResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />
+                )
+            }
             </div>
         </div>
     );
